9ad935f6a92411bc9d5c5d1e49881b082d54dfb0
Danielle Barros Bassetto <danibbassetto@hotmail.com>
2023-05-01 23:53:25 -0300

    new

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -1,4 +1,5 @@
-﻿using FilmesApi.Models;
+﻿using FilmesApi.Data;
+using FilmesApi.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesApi.Controllers;
@@ -7,19 +8,34 @@ namespace FilmesApi.Controllers;
 [Route("[controller]")]
 public class FilmeController : ControllerBase
 {
-    private static List<Filme> filmes = new List<Filme>();
+    private FilmeContext _context;
+
+    public FilmeController(FilmeContext context)
+    {
+        _context = context;
+    }
 
     [HttpPost]
-    public void AdicionaFilme([FromBody] Filme filme)
+    public IActionResult AdicionaFilme([FromBody] Filme filme)
     {
-        filmes.Add(filme);
-        Console.WriteLine(filme.Titulo);
-        Console.WriteLine(filme.Duracao);
+        _context.Filmes.Add(filme);
+        _context.SaveChanges();
+        // CreatedAtAction mostra o location de onde está o filme criado
+        return CreatedAtAction(nameof(RecuperaFilmePorId), new { id = filme.Id}, filme);
     }
 
     [HttpGet]
-    public IEnumerable<Filme> RecuperaFilmes()
+    public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0, [FromQuery] int take = 50)
     {
-        return filmes;
+        return _context.Filmes.Skip(skip).Take(take); 
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperaFilmePorId(int id)
+    {
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme == null)
+            return NotFound();
+        return Ok(filme);
     }
 }
--- FilmesApi/Models/Filme.cs
+++ FilmesApi/Models/Filme.cs
@@ -4,6 +4,9 @@ namespace FilmesApi.Models;
 
 public class Filme
 {
+    [Key]    
+    [Required]
+    public int Id { get; set; }
     [Required(ErrorMessage = "O campo título é obrigatório")]
     public string Titulo { get; set; }
     [Required(ErrorMessage = "O campo gênero é obrigatório")]
